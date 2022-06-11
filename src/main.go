3b22538245924dbf402ce0567cab89001fc494b2
changjjjjjjj <light9632@gmail.com>
2022-06-11 20:39:22 +0800

    [feat] Add user service server

--- src/main.go
+++ src/main.go
@@ -2,33 +2,48 @@ package main
 
 import (
 	"context"
-	proto "github.com/theraffle/backend/src/genproto/pb"
+	"github.com/theraffle/backend/src/genproto/pb"
 	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
 func main() {
-	grpcServer := grpc.NewServer()
-	proto.RegisterUserManagerServer(grpcServer, &userManagerServer{})
+	run()
 }
 
-type userManagerServer struct{}
+func run() string {
+	l, err := net.Listen("tcp", "3550")
+	if err != nil {
+		log.Fatal(err)
+	}
+	srv := grpc.NewServer()
 
-func (u *userManagerServer) LogoutUser(ctx context.Context, request *proto.LogoutUserRequest) (*proto.Error, error) {
+	svc := &userService{}
+
+	pb.RegisterUserServiceServer(srv, svc)
+	go srv.Serve(l)
+	return l.Addr().String()
+}
+
+type userService struct{}
+
+func (u *userService) LogoutUser(ctx context.Context, request *pb.LogoutUserRequest) (*pb.Error, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (u *userManagerServer) CreateUser(ctx context.Context, request *proto.CreateUserRequest) (*proto.Error, error) {
+func (u *userService) CreateUser(ctx context.Context, request *pb.CreateUserRequest) (*pb.Error, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (u *userManagerServer) GetUser(ctx context.Context, request *proto.GetUserRequest) (*proto.GetUserResponse, error) {
+func (u *userService) GetUser(ctx context.Context, request *pb.GetUserRequest) (*pb.GetUserResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (u *userManagerServer) LoginUser(ctx context.Context, request *proto.LoginUserRequest) (*proto.Error, error) {
+func (u *userService) LoginUser(ctx context.Context, request *pb.LoginUserRequest) (*pb.Error, error) {
 	//TODO implement me
 	panic("implement me")
 }
